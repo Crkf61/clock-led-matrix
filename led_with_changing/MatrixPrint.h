c056c2c3cdb9b727e24a53d88e10f9a49adc028c
Crkf61 <chrislotery1@gmail.com>
2021-09-28 22:24:52 +0100

    add folder for complete clock and populate with existing code

---
+++ led_with_changing/MatrixPrint.h
@@ -0,0 +1,66 @@
+#include <MD_MAX72xx.h>
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES  4
+
+#define CLK_PIN   13  // or SCK
+#define DATA_PIN  11  // or MOSI
+#define CS_PIN    10  // or SS
+
+
+class MatrixPrinter {
+
+MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Arbitrary pins
+//MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+String _displayed_message;
+
+public:
+
+  MatrixPrinter() {
+    mx.begin();
+    mx.control(MD_MAX72XX::INTENSITY, 0);
+    //mx.update(MD_MAX72XX::OFF);
+  }
+
+  bool printIfNeeded(String message)
+  // only print if message is different to _displayed_message
+  {
+    bool different = false;
+    for (int i=0; i<5; i++) {
+      if (message[i] != _displayed_message[i]) different = true;
+    }
+
+    if (different) {
+      printString(message);
+      _displayed_message = message;
+    }
+    return different;
+  }
+
+  void printString(String message)
+  // print string of length 5 to led matrix
+  {
+    mx.clear(3);
+    //mx.update(MD_MAX72XX::OFF);
+    mx.setChar((4*COL_SIZE-1), message[0]);
+    mx.clear(2);
+    mx.setChar((3*COL_SIZE+1), message[1]);
+    mx.setChar((2*COL_SIZE+1), message[2]);
+    mx.clear(1);
+    mx.setChar((2*COL_SIZE-4), message[3]);
+    mx.clear(0);
+    mx.setChar(COL_SIZE-4, message[4]);
+
+    mx.update();
+
+    //mx.update(MD_MAX72XX::ON);
+  }
+
+};
---
+++ led_with_changing/led_with_changing.ino
@@ -0,0 +1,78 @@
+#include "MatrixPrint.h"
+#include "TimeString.h"
+
+
+MatrixPrinter mxprinter;
+
+int selectPin = 6;
+int finishPin = 3;
+
+volatile bool changing = false;
+volatile bool just_changed = false;
+
+void stop_changing() {
+  changing = false;
+  just_changed = true;
+}
+
+void setup() {
+  //digitalWrite(LED_BUILTIN, HIGH);
+  attachInterrupt(digitalPinToInterrupt(finishPin), stop_changing, RISING);
+}
+
+
+
+void loop() {
+  RTCTimeString timestring;
+  String time_message = timestring.getTimeStr();
+  bool changed_display = mxprinter.printIfNeeded(time_message);
+  digitalWrite(12, changed_display);
+  delay(25);
+  if (digitalRead(selectPin)) {
+    changing = true;
+  }
+
+  DateTime today;
+  static int hour_min[2];
+  bool _changing_hour;
+  if (changing) {
+    today = timestring.getNow();
+    hour_min[0] = today.hour();
+    hour_min[1] = today.minute();
+    _changing_hour = true;
+    mxprinter.printIfNeeded("^^   ");
+    delay(500);
+  }
+  while (changing) {
+    int change_state;
+    if (digitalRead(selectPin)) _changing_hour = !_changing_hour;
+    change_state = timestring.adjustTime(hour_min, _changing_hour);
+    time_message = timestring.getChangeStr(hour_min);
+    mxprinter.printIfNeeded(time_message);
+
+    if (change_state == 1) // select
+    {
+      if (_changing_hour) mxprinter.printIfNeeded("^^   ");
+      else mxprinter.printIfNeeded("   ^^");
+      delay(500);
+    }
+    else if (change_state == 2 || change_state == 3) // plus or minus
+    {
+      delay(100);
+    }
+    else if (change_state == 0) // no button press. don't wait so long)
+    {
+      delay(25);
+    }
+    
+
+  }
+  if (just_changed) 
+  // need to adjust RTC
+  {
+    timestring.finish_adjusting(hour_min);
+    just_changed = false;
+  }
+
+
+}
